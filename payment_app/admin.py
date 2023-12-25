de9a618df16faa290685d2007c4e41fc7f243f52
lllrikitikilll <lllrikitikilll@yandex.ru>
2023-12-25 22:00:12 +0300

    refactor admin

--- payment_app/admin.py
+++ payment_app/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.db.models import Sum
+from payment_app.models import Item, Order, Discount, Tax
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'tax']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'discount', 'order_count', 'order_sum', 'percent_off', 'order_sum_with_discount']
+    list_display_links = ['id', 'order_sum']
+
+    @staticmethod
+    def order_count(obj):
+        return obj.items.count()
+
+    @staticmethod
+    def order_sum(obj):
+        return obj.items.aggregate(res=Sum("price"))['res']
+
+    @staticmethod
+    def percent_off(obj):
+        return obj.discount.percent_off
+
+    @staticmethod
+    def order_sum_with_discount(obj):
+        sum = OrderAdmin.order_sum(obj)
+        return sum - sum * (obj.discount.percent_off / 100)
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'percent_off', 'valid')
+    list_display_links = ['name']
+    readonly_fields = ('id', 'created')
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('id', 'display_name', 'percentage')
+    readonly_fields = ('tax_id',)
